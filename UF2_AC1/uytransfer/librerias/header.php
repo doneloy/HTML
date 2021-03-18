912598ff7dbfeeeaa156ddda76a9efb7df66e07c
Eloy Rastrero <elorast@gmail.com>
2021-03-18 17:18:58 +0100

    creación header

---
+++ UF2_AC1/uytransfer/librerias/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+
+    <title>Uy!Transfer</title>
+  </head>
+  <body>
+
+      <div id="prim">
+        <h1 id="uy">Uy!Transfer</h1>
+
+        <div id="link">
+          <a href="upload.php">Enviar archivo</a>
+          <a href="upload.php">Mis últimos archivos</a>
+        </div>
+        
+      </div>
+
+  </body>
+</html>
+
+    
