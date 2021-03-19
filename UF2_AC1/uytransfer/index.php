0338d9e9a0abfc8e9eb9781f33a5ef7cdcec85ea
Eloy Rastrero <elorast@gmail.com>
2021-03-19 18:42:44 +0100

    creación del header.php

---
+++ UF2_AC1/uytransfer/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+
+    <title>Uy!Transfer</title>
+  </head>
+  <body>
+
+    <?php
+
+      include "librerias/header.php";
+
+    ?>
+  </body>
+</html>
