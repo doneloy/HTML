c2541eebf7dccd3785c82677ec70fe25bef43ca2
Eloy Rastrero <elorast@gmail.com>
2021-03-23 16:48:03 +0100

    diseño acabado

--- UF2_AC1/uytransfer/index.php
+++ UF2_AC1/uytransfer/index.php
@@ -16,12 +16,12 @@
 
     <div id="formulario">
       <form id="form">
-        <input type="name" name="nombre" placeholder="Tu nombre"><br>
-        <input type="file" name="archivo"><br>
-        <input type="checkbox" name="email"><label>Quiero enviar el link de descarga por email</label><br>
-        <input type="email" name="email" placeholder="Email del destinatario"><br>
-        <label>Mensaje</label><br>
-        <input type="text" name="mensaje"><br>
+        <input type="name" name="nombre" placeholder="Tu nombre" style="width: 40%;"><br><br>
+        <input type="file" name="archivo"><br><br>
+        <input type="checkbox" name="email"><label>Quiero enviar el link de descarga por email</label><br><br>
+        <input type="email" name="email" placeholder="Email del destinatario" style="width: 40%;"><br><br>
+        <label>Mensaje</label><br><br>
+        <input type="text" name="mensaje" style="width: 40%; height: 100px;"><br><br>
         <input type="submit" value="Subir archivo">
       </form>
     </div>
