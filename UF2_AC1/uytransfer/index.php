802ff46e76f05f7d4163b49dc0cb9f6a1332bbda
Eloy Rastrero <elorast@gmail.com>
2021-03-28 19:44:38 +0200

    creación de upload.php

--- UF2_AC1/uytransfer/index.php
+++ UF2_AC1/uytransfer/index.php
@@ -15,14 +15,14 @@
     ?>
 
     <div id="formulario">
-      <form id="form">
+      <form name="subir" id="form" action="upload.php" method="post" enctype="multipart/form-data">
         <input type="name" name="nombre" placeholder="Tu nombre" style="width: 40%;"><br><br>
         <input type="file" name="archivo"><br><br>
         <input type="checkbox" name="email"><label>Quiero enviar el link de descarga por email</label><br><br>
-        <input type="email" name="email" placeholder="Email del destinatario" style="width: 40%;"><br><br>
+        <input type="mail" name="email" placeholder="Email del destinatario" style="width: 40%;"><br><br>
         <label>Mensaje</label><br><br>
-        <input type="text" name="mensaje" style="width: 40%; height: 100px;"><br><br>
-        <input type="submit" value="Subir archivo">
+        <textarea name="mensaje" style="width: 40%; height: 100px;"></textarea><br><br>
+        <button type="submit">Subir archivo</button>
       </form>
     </div>
     
---
+++ UF2_AC1/uytransfer/upload.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/upload.css">
+    <title>Uy!Transfer</title>
+  </head>
+  <body>
+
+    <?php
+
+      include "librerias/header.php";
+
+    ?>
+
+    <div id="grande">
+    	<img src="images/correcto.png">
+    	<h3>Archivo enviado correctamente</h3>
+    </div>
+
+    
+  </body>
+</html>
